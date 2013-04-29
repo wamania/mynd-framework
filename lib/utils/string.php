5f87dd0d21957c360b899ecc845a0955301373ee
wamania <wamania@gmail.com>
2013-04-29 14:19:14 +0200

    ajout util string

---
+++ lib/utils/string.php
@@ -0,0 +1,24 @@
+<?php
+
+class MfString
+{
+    public static function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        return $text;
+    }
+}
